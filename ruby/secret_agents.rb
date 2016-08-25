0f67a4f714f7af6b55749e5bed7db48672c2a300
Robert Turner Jr <georob23@gmail.com>
2016-08-24 19:21:47 -0700

    Create pseudeocode for Encrypt and Decrypt methods.

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,31 @@
+=begin
+ENCRYPT
+1.get string from user
+2.Start at index 0 and loop through every character
+3. increment character by one value (.next)
+4. delete first character(.delete)
+5. increment the index value by one. (loop to next character)
+6. if character == nil then exit loop. or use .length
+7. Print string
+
+DECRYPT
+1. Define an reference string of "abcdefghijklmnopqrstuvwxyz"
+2. Get string from user
+3. Start at index 0 and loop through every character
+4. Get character, get character index of character from reference string
+5. Subract 1 from returned reference string index value
+6. Set character in input string to str[0] = reference[reference value - 1]
+7. Move to next character in input string until 'nil' is reached. 
+
+=end
+
+puts "Type a word for encrypting:"
+user_string = gets.chomp
+
+def encrypt(string)
+  # 2.Start at index 0 and loop through every character
+  # 3. increment character by one value (.next)
+  # 4. delete first character(.delete)
+  # 5. increment the index value by one. (loop to next character)
+  # 6. if character == nil then exit loop. or use .length
+  # 7. Print string

5d1caeef3d87be3ed7fe28eaa3305867fc5357de
Robert Turner Jr <georob23@gmail.com>
2016-08-24 19:53:20 -0700

    Add Decrypt method.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -20,7 +20,7 @@ DECRYPT
 =end
 
 puts "Type a word for encrypting:"
-user_string = gets.chomp
+encrypt_string = gets.chomp
 
 def encrypt(string)
   # 2.Start at index 0 and loop through every character
@@ -29,6 +29,8 @@ def encrypt(string)
   # 3. increment character by one value (.next)
     if string[idx] == "z"
       string[idx] = "a"
+    elsif string[idx] == " "
+      string[idx] = " "
     else
       string[idx] = string[idx].next
     end 
@@ -37,5 +39,32 @@ def encrypt(string)
   string
 end
 
+puts encrypt(encrypt_string)
 
-puts encrypt(user_string)
+puts "Type a word for decrypting:"
+decrypt_string = gets.chomp
+
+def decrypt(string)
+  # 1. Define an reference string of "abcdefghijklmnopqrstuvwxyz"
+  ref_string = "abcdefghijklmnopqrstuvwxyz"
+  # 3. Start at index 0 and loop through every character
+  idx = 0
+  while idx < string.length
+    if string[idx] == " "
+      string[idx] = " "
+    else
+      ref_idx = ref_string.index(string[idx])
+      string[idx] = ref_string[ref_idx - 1]
+    end
+    idx += 1
+  end
+  string
+end
+
+puts decrypt(decrypt_string)
+
+  # 4. Get character, get character index of character from reference string
+  # 5. Subract 1 from returned reference string index value
+  # 6. Set character in input string to str[0] = reference[reference value - 1]
+  # 7. Move to next character in input string until 'nil' is reached. 
+

b90a2d99e5efdae9d76cf3a57990edef0de9ffa5
Robert Turner Jr <georob23@gmail.com>
2016-08-30 08:42:27 -0700

    Add pseudocode to hashes.rb

---
+++ ruby/hashes.rb
@@ -0,0 +1,15 @@
+# DBC Challenge 5.2
+
+# Create empty hash to store client data with relevant keys and nil values
+
+# Prompt the user for data to populate all key:value pairs
+# Convert user input into appropriate data type before assigning values to keys
+
+# Print keys and values out in an easily readable format 
+
+# Ask user if any values need to be changed. If so, prompt user to ask 
+# which value to change. Use case statement to change chosen value.
+
+# Print final keys and values out in an easily readable format and thank
+# the user for using the program
+

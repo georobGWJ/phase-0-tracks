dcab4198cfab111450fe88489bdeed083a5d2438
Robert Turner Jr <georob23@gmail.com>
2016-08-30 10:24:37 -0700

    Add code to allow user to change incorrect data.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -31,8 +31,8 @@ client_data[:house_sq_ft] = gets.chomp.to_f
 
 puts "\nDo you have cats or dogs or turtles or other kinds of pets?"
 case gets.chomp.downcase
-    when "yes", "y" then client_data[:has_pets] = true 
-    when "no", "n" then client_data[:has_pets] = false
+  when "yes", "y" then client_data[:has_pets] = true 
+  when "no", "n" then client_data[:has_pets] = false
 end
 
 puts "\nWhat is your preferred decor?"
@@ -47,8 +47,8 @@ puts "------------------------------------------------------------\n"
 
 puts "%-20s| %s" % 
   ["Name", client_data[:first_name] + " " + client_data[:surname]]
-puts "%-20s| %2d" % ["Age", client_data[:age]]
-puts "%-20s| %1d" % ["Number of Children", client_data[:num_children]]
+puts "%-20s| %d" % ["Age", client_data[:age]]
+puts "%-20s| %d" % ["Number of Children", client_data[:num_children]]
 puts "%-20s| %1.1f" % ["Home Square Footage", client_data[:house_sq_ft]]
 puts "%-20s| %s" % ["Pet Owner", client_data[:has_pets].to_s.capitalize]
 puts "%-20s| %s" % ["Preferred Decor", client_data[:decor_pref].capitalize]
@@ -58,9 +58,61 @@ puts "\n============================================================\n"
 # which value to change. Use case statement to change chosen value.
 puts "Is this correct?"
 case gets.chomp.downcase
-    when "yes", "y" then puts "Great!" 
-    when "no", "n" then puts "Sorry!"
+  when "yes", "y"
+    puts "Great!" 
+  when "no", "n"
+    puts "Sorry! What would you like to change?"
+    case gets.chomp.downcase
+      when "name"
+        puts "\nPlease enter your full name"
+        fullname = gets.chomp.split
+        client_data[:first_name] = fullname[0].to_s.capitalize
+        if fullname.length > 2
+          client_data[:middle_name] = fullname[1].to_s.capitalize
+          client_data[:surname] = fullname[2].to_s.capitalize
+        elsif fullname.length == 2
+          client_data[:middle_name] = ""
+          client_data[:surname] = fullname[1].to_s.capitalize
+        end
+
+      when "age"
+        puts "\nHow old are you?"
+        client_data[:age] = gets.chomp.to_i
+
+      when "children", "number of children"
+        puts "\nHow many chilren do you have?"
+        client_data[:num_children] = gets.chomp.to_i
+
+      when "square footage", "home square footage"
+        puts "\nWhat is the square footage of your home?"
+        client_data[:house_sq_ft] = gets.chomp.to_f
+
+      when "pet owner"
+        puts "\nDo you have cats or dogs or turtles or other kinds of pets?"
+        case gets.chomp.downcase
+          when "yes", "y" then client_data[:has_pets] = true 
+          when "no", "n" then client_data[:has_pets] = false
+        end
+
+      when "decor", "preferred decor"
+        puts "\nWhat is your preferred decor?"
+        client_data[:decor_pref] = gets.chomp.to_s
+    end
 end
+
 # Print final keys and values out in an easily readable format and thank
 # the user for using the program
 
+puts "\n============================================================\n"
+puts "Thank you #{client_data[:first_name]}! This is the Truth.\n"
+puts "------------------------------------------------------------\n"
+puts "%-20s| %s" % 
+  ["Name", client_data[:first_name] + " " + client_data[:surname]]
+puts "%-20s| %d" % ["Age", client_data[:age]]
+puts "%-20s| %d" % ["Number of Children", client_data[:num_children]]
+puts "%-20s| %1.1f" % ["Home Square Footage", client_data[:house_sq_ft]]
+puts "%-20s| %s" % ["Pet Owner", client_data[:has_pets].to_s.capitalize]
+puts "%-20s| %s" % ["Preferred Decor", client_data[:decor_pref].capitalize]
+puts "============================================================\n"
+
+puts "We look forward to working with you to decorate your house!!!"

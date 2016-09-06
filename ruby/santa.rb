80614da30b3c0f92000fc0c5df70e8cc2da640ef
Robert Turner Jr <georob23@gmail.com>
2016-09-06 16:44:03 -0700

    Add attribute-changing and getter methods to Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -10,13 +10,37 @@ class Santa
     @age = 0
     puts "Initializing Santa instance ..."
   end
+  
+  # Setter method(s)
+  def gender=(new_gender)
+    @gender = new_gender
+  end
 
+  # Getter method(s)
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  # Other methods
   def speak
     puts "Ho, ho, ho! Haaappy holidays!"
   end
 
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
+  end
+
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking << reindeer
   end
 
 end
@@ -32,9 +56,15 @@ santas = []
 
 santa_applicants.each { | gend, ethn | santas << Santa.new(gend, ethn) }
 
-santas.each { |santa| p santa}
-# al = Santa.new("male", "ambiguous")
+#santas.each { |santa| p santa}
+
+al = Santa.new("male", "black")
+p al
 # al.speak
 # al.eat_milk_and_cookies("Oreo")
+al.celebrate_birthday
+al.get_mad_at("Rudolph")
+al.gender = "bigender"
+p al
 
-# p al
+puts "Santa is #{al.age} years old and is #{al.ethnicity}."

b6d165760bce0e0e53ef53d71951a2df60922e2e
Robert Turner Jr <georob23@gmail.com>
2016-09-05 19:58:52 -0700

    Refactor code and comment out test lines.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -9,57 +9,49 @@
     # Turn indexes into keys
     # Set the values to our default variables, all start at zero
 
-
-def create_hash(items)
-  groceries = {}
-  default_qty = 0
-  items = items.split
-
-  items.each do |food|
-    groceries[food] = default_qty
-  end
-  
-  groceries    
-end
-
-
 # Method to add an item to a list
 # input: item name and optional quantity
 # steps:
 # output: Hash with items and values of zero
-def add_item(groceries, food, qty = 0)
-  groceries[food] = qty
-end
-
 
 # Method to remove an item from the list
 # input: item name
 # steps:
   # Use delete method to remove item from hash
 # output: Hash
-def remove_item(groceries, food)
-  groceries.delete(food)
-end
 
 # Method to update the quantity of an item
 # input: Item we want to replace and the new quantity
 # steps:
   # Set new value to new input key
 # output: Hash
-def update_qty(groceries, food, qty)
-  groceries[food] = qty
-end
-
 
 # Method to print a list and make it look pretty
 # input: Hash Name
 # steps:
- # Iterate through the list to print out each item and quantity
+  # Iterate through the list to print out each item and quantity
 # output:
+
+def create_hash(items, default_qty = 0)
+  groceries = {}
+  items.split.each { |food| groceries[food] = default_qty }
+  groceries    
+end
+
+def add_item(groceries, food, qty = 0)
+  groceries[food] = qty
+end
+
+def remove_item(groceries, food)
+  groceries.delete(food)
+end
+
+def update_qty(groceries, food, qty)
+  add_item(groceries, food, qty)
+end
+
 def pretty_print(groceries)
-  groceries.each do |key, value|
-    puts "You have #{value} #{key}(s)."
-  end
+  groceries.each { |food, qty| puts "You have #{qty} #{food}(s)." }
 end
 
 

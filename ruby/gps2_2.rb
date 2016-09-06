26175ce0fc7643970675f5e94faa46b37d4570a1
Robert Turner Jr <georob23@gmail.com>
2016-09-05 19:06:30 -0700

    Create pseudocode

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,1 +1,41 @@
 # DBC GPS 2.2
+
+# Method to create a hash
+def create_hash(items)
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+  # set default quantity
+  # Create variable set to zero
+  default_qty = 0
+  # Split each word into an index that creates an array
+  items = items.split
+  groceries 
+  # Turn indexes into keys
+  # Set the values to our default variables, all start at zero
+  
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?]
+  # Hash
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps:
+# output: Hash with items and values of zero
+
+# Method to remove an item from the list
+# input: item name
+# steps:
+  # Use delete method to remove item from hash
+# output: Hash
+
+# Method to update the quantity of an item
+# input: Item we want to replace and the new quantity
+# steps:
+  # Set new value to new input key
+# output: Hash
+
+# Method to print a list and make it look pretty
+# input: Hash Name
+# steps:
+ # Iterate through the list to print out each item and quantity
+# output:

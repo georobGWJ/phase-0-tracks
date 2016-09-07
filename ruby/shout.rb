0f38b69cbab10f52b9e4b7187e0d03acf3473424
Robert Turner Jr <georob23@gmail.com>
2016-09-07 16:24:53 -0700

    Modify Shout Module into a mixin Module. Add test lines.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,18 +1,51 @@
 # DBC Challenge 6.4
 
-module Shout
+# module Shout
   
-  def self.yell_angrily(words)
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     "\xE2\x99\xA5 " + words + "! \xE2\x99\xA5"
+#   end
+
+# end
+
+# Test Driver Code for stand alone Shout Module
+# p Shout.yell_angrily("I hate trains")
+
+# p Shout.yelling_happily("I love cheeseburgers")
+
+module Shout
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     "\xE2\x99\xA5 " + words + "! \xE2\x99\xA5"
   end
-
 end
 
-# Test Driver Code
-p Shout.yell_angrily("I hate trains")
+class The_Isley_Brothers
+  include Shout
+end
 
-p Shout.yelling_happily("I love cheeseburgers")
+class Tears_For_Fears
+  include Shout
+end
+
+# Test Driver Code for mixin Shout Module
+brothers = The_Isley_Brothers.new
+puts brothers.yelling_happily("You know you make me wanna (Shout!)")
+puts brothers.yelling_happily("Kick my heels up and (Shout!)")
+puts brothers.yelling_happily("Throw my hands up and (Shout!)")
+puts brothers.yelling_happily("Throw my head back and (Shout!)")
+puts brothers.yelling_happily("Come on now (Shout!)")
+puts "\n\n"
+
+tears = Tears_For_Fears.new
+puts tears.yell_angrily("Shout, shout, let it all out")
+puts tears.yell_angrily("These are the things I can do without")
+puts tears.yell_angrily("Come on")
+puts tears.yell_angrily("I'm talking to you, so come on")

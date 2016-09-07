f311861cedf8d3802619488df2ae7de52ad0ef59
Robert Turner Jr <georob23@gmail.com>
2016-09-06 18:25:51 -0700

    Add Kitten class with .eats and .sleeps methods and a loop to create many, many kittens.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,9 @@
 class Puppy
 
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -23,9 +27,42 @@ class Puppy
 
 end
 
-gizmo = Puppy.new
-gizmo.fetch("ball")
-gizmo.speak(3)
-gizmo.roll_over
-puts gizmo.dog_years(4)
-gizmo.high_five
+# Test Driver Code
+# gizmo = Puppy.new
+# gizmo.fetch("ball")
+# gizmo.speak(3)
+# gizmo.roll_over
+# puts gizmo.dog_years(4)
+# gizmo.high_five
+
+class Kitten
+
+  def initialize
+    puts "You have initialized a new kitten!"
+  end
+
+  def eats(food)
+    puts "Your kitten eats #{food} and meows for more."
+  end
+
+  def sleeps(hours)
+    puts "Your kitten took a #{hours} hour long nap in the sun."
+  end
+
+end
+
+num_of_kittens = 50
+kittens = []
+
+num_of_kittens.times { kittens << Kitten.new }
+
+kittens.each do |kitten|
+  kitten.eats("IAMs cat food")
+  kitten.sleeps(rand(0..3))
+end
+
+
+# Test Driver Code
+# berkeley = Kitten.new
+# berkeley.eats("tuna")
+# berkeley.sleeps(2)

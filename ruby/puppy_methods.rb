326e0c3fbb0be60609ceb95a7fd0d3af01459d42
Robert Turner Jr <georob23@gmail.com>
2016-09-06 18:08:03 -0700

    Add methods to puppy_methods for Release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times { puts "Woof!" }
+  end
+
+  def roll_over
+    puts "**rolls over**"
+  end
+
+  def dog_years(human_years)
+    human_years * 7
+  end
+
+  def high_five
+    puts "Your puppy high fives you!"
+  end
+
 end
 
+gizmo = Puppy.new
+gizmo.fetch("ball")
+gizmo.speak(3)
+gizmo.roll_over
+puts gizmo.dog_years(4)
+gizmo.high_five

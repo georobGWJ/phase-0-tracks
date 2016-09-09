e5aa23ed20ad87c38be5792bbd799a3ad89a034b
Robert Turner Jr <georob23@gmail.com>
2016-09-09 11:49:39 -0700

    Implement WordGame initialize method and RSpec tests.

--- ruby/word-game/WordGame.rb
+++ ruby/word-game/WordGame.rb
@@ -1,20 +1,39 @@
 # Define the Word_Game class 
+class WordGame
+
+attr_accessor :player1, :player2
+attr_reader :phrase, :chars_guessed, :guesses_made, :guesses_allowed
 
 # Initialize the Class
 #   set class variable goal_phrase, this is passed in as an argument
+  def initialize(phrase, player1, player2)
+    @phrase = phrase
+    @player1, @player2 = player1, player2
+#   set class phrase nested array that stores each character in the 
+#   goal phrase and a false flag. When printing the partially guessed phrase, 
+#   this flag will track if the user has guessed that character. 
+    @chars_guessed = []
+    @phrase.split('').each { |char| chars_guessed << [char, false] }
 
-#   set class phrase nested array that stores each character in the goal_phrase and a false flag. When printing the partially guessed phrase, this flag will track if the user has guessed that character. For example the nested array for 'Nest' would be:
-#   [['N', false], ['e', false], ['s', false], ['t', false]]
-
-#   set class variable array phrases_guessed to track what the user has already guessed
+#   set class variable array phrases_guessed to track what the user has already 
+#   guessed
+    @phrases_guessed = []
 
 #   set class int variable guesses_made set to 0 to track how many guesses have been made
+    @guesses_made = 0
 
-#   set class int variable guesses_allowed using some logic to give more guesses for longer names
+#   set class int variable guesses_allowed using some logic to give more 
+#   guesses for longer names
+    @guesses_allowed = 4 + @phrase.length / 2
+
+  end
+
 
 # Print Phrase Method
 #   Create a string by looping through the phrase nested array and appending the correct character if the flag is true and '_' if it is false
 #   Return this string
+def pretty_print
+end
 
 # User Guess Method
 #   Takes a string argument as input.
@@ -29,10 +48,13 @@
 #   Call self Print method
 
 #   If the number of guesses equals the number of guesses allowed call self Lose method
-
+def user_guess
+end
 
 # User Win Method
 #   Print a You Win! message
 
 # User Lose Method
 #   Print a You Lose! message
+
+end
--- ruby/word-game/word_game_spec.rb
+++ ruby/word-game/word_game_spec.rb
@@ -1,11 +1,17 @@
 require_relative 'WordGame'
 
-describe TodoList do
+describe WordGame do
 
-  let(:game) { WordGame.new("alligator") }
+  let(:game) { WordGame.new("alligator", "Vinny", "Austin") }
 
-  it "does a thing" do
-    expect(game.method).to eq something
+  it "Initializes Game State properly" do
+    expect(game.phrase).to eq "alligator"
+    expect(game.player1).to eq "Vinny"
+    expect(game.player2).to eq "Austin"
+    expect(game.chars_guessed).to eq [['a',false],['l',false],['l',false],
+      ['i',false],['g',false],['a',false],['t',false],['o',false],['r',false]]
+    expect(game.guesses_made).to eq 0
+    expect(game.guesses_allowed).to eq 8
   end
-  
+
 end

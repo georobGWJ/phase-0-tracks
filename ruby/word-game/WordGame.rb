3fd0a5f58f34d297ebf9e53935502a1ecb6a9485
Robert Turner Jr <georob23@gmail.com>
2016-09-10 10:05:16 -0700

    Clean up comments and formatting.

--- ruby/word-game/WordGame.rb
+++ ruby/word-game/WordGame.rb
@@ -1,40 +1,76 @@
+# PSEUDOCODE
 # Define the Word_Game class 
+  # Initialize the Class
+    #   set class variables phrase, player1 and player2 
+    #   these are passed in as arguments
+
+    #   set class phrase nested array that stores each character in the 
+    #   goal phrase and a false flag. When printing the partially guessed phrase, 
+    #   this flag will track if the user has guessed that character. 
+
+    #   set class variable array phrases_guessed to track what the user has already 
+    #   guessed
+
+    #   set class int variable guesses_made set to 0 to track how many guesses 
+    #   have been made
+  
+    #   set class int variable guesses_allowed using some logic to give more 
+    #   guesses for longer names
+
+    #   set game_over flag
+    #   Initialize returns false by default
+
+  # Pretty Print Phrase Method
+    #   Create a string by looping through the phrase nested array and appending 
+    #   the correct character if the flag is true and '_' if it is false
+    #   Pretty Print Returns this string
+
+  # User Guess Method
+    #   Takes a string argument as input.
+
+    #   If the guess hasn't been made before, add the string to the 
+    #   phrases_guessed array and increment guesses_made
+
+    #   Attempt direct comparison of the input phrase and the goal phrase. If 
+    #   they match call self Win method.
+
+    #   Otherwise, set a counter to 0 and loop through both the phrase guessed 
+    #   characters and the phrase nested array. Where characters in both match, 
+    #   change the flag in the nested array to true.
+
+    #   Call self pretty_print method
+
+    #   If the number of guesses equals the number of guesses allowed call 
+    #   self Lose method
+    #   User Guess Returns nil by default
+
+  # User Win Method
+  #   Print a You Win! message and set game_over flag to true
+
+  # User Lose Method
+  #   Print a You Lose! message and set game_over flag to true
+
+
+  
 class WordGame
 
   attr_accessor :player1, :player2
   attr_reader :phrase, :chars_guessed, :guesses_made, :guesses_allowed,
               :phrases_guessed, :game_over
 
-  # Initialize the Class
-  #   set class variable goal_phrase, this is passed in as an argument
-    def initialize(phrase, player1, player2)
+    def initialize(phrase, player1 = "Player 1", player2 = "Player 2")
       @phrase = phrase
       @player1, @player2 = player1, player2
-  #   set class phrase nested array that stores each character in the 
-  #   goal phrase and a false flag. When printing the partially guessed phrase, 
-  #   this flag will track if the user has guessed that character. 
       @chars_guessed = []
       @phrase.split('').each { |char| chars_guessed << [char, false] }
 
-  #   set class variable array phrases_guessed to track what the user has already 
-  #   guessed
       @phrases_guessed = []
-
-  #   set class int variable guesses_made set to 0 to track how many guesses have been made
       @guesses_made = 0
-
-  #   set class int variable guesses_allowed using some logic to give more 
-  #   guesses for longer names
       @guesses_allowed = 4 + @phrase.length / 2
-
       @game_over = false
     end
 
 
-  # Print Phrase Method
-  #   Create a string by looping through the phrase nested array and appending 
-  #   the correct character if the flag is true and '_' if it is false
-  #   Return this string
   def pretty_print
     pretty_str = ""
     @chars_guessed.each do |idx|
@@ -47,24 +83,6 @@ class WordGame
     pretty_str
   end
 
-  # User Guess Method
-  #   Takes a string argument as input.
-
-  #   If the guess hasn't been made before, add the string to the 
-  #   phrases_guessed array and increment guesses_made
-  #   Otherwise just continue
-
-  #   Attempt direct comparison of the input phrase and the goal phrase. If 
-  #   they match call self Win method.
-
-  #   Otherwise, set a counter to 0 and loop through both the phrase guessed 
-  #   characters and the phrase nested array. Where characters in both match, 
-  #   change the flag in the nested array to true.
-
-  #   Call self Print method
-
-  #   If the number of guesses equals the number of guesses allowed call 
-  #   self Lose method
   def user_guess(guess)
     if !@phrases_guessed.include? guess 
       @phrases_guessed << guess
@@ -89,8 +107,6 @@ class WordGame
 
   end
 
-  # User Win Method
-  #   Print a You Win! message
   def win()
     puts "\nYou're a big winner. I'm gonna ask you a simple question and I" 
     puts "want you to listen to me: who's the big winner here tonight?" 
@@ -100,8 +116,6 @@ class WordGame
   end
 
 
-  # User Lose Method
-  #   Print a You Lose! message
   def lose()
     puts "\nYou had #{@guesses_allowed} guesses...  #{@guesses_allowed}, #{player2}!"
     puts "It looks like #{player1} outsmarted you."

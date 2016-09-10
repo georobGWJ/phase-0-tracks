943092ef713ebafbeced4ac142899cbcbc1c4a84
Robert Turner Jr <georob23@gmail.com>
2016-09-10 09:50:43 -0700

    Add driver code to run game.

--- ruby/word-game/WordGame.rb
+++ ruby/word-game/WordGame.rb
@@ -3,7 +3,7 @@ class WordGame
 
   attr_accessor :player1, :player2
   attr_reader :phrase, :chars_guessed, :guesses_made, :guesses_allowed,
-              :phrases_guessed
+              :phrases_guessed, :game_over
 
   # Initialize the Class
   #   set class variable goal_phrase, this is passed in as an argument
@@ -26,6 +26,8 @@ class WordGame
   #   set class int variable guesses_allowed using some logic to give more 
   #   guesses for longer names
       @guesses_allowed = 4 + @phrase.length / 2
+
+      @game_over = false
     end
 
 
@@ -90,18 +92,57 @@ class WordGame
   # User Win Method
   #   Print a You Win! message
   def win()
-    puts "You're a big winner. I'm gonna ask you a simple question and I" 
+    puts "\nYou're a big winner. I'm gonna ask you a simple question and I" 
     puts "want you to listen to me: who's the big winner here tonight?" 
     puts "Huh? #{player2}, that's who. #{player2}'s the big winner."
     puts "#{player2} wins!"
+    @game_over = true
   end
 
 
   # User Lose Method
   #   Print a You Lose! message
   def lose()
-    puts "You had #{@guesses_allowed} guesses...  #{@guesses_allowed}, #{player2}!"
-    puts "It looks like that wasn't enough."
+    puts "\nYou had #{@guesses_allowed} guesses...  #{@guesses_allowed}, #{player2}!"
+    puts "It looks like #{player1} outsmarted you."
+    @game_over = true
+  end
+
+end
+
+# Driver Code
+
+puts "Welcome to the Wondeful, Wooly and Wacky World of Words!!!"
+puts "==========================================================\n"
+
+while true
+  puts "Would you like to play a game?"
+  puts "There should be two of you:"
+  puts "One devious devil defining the phrase (Player 1), and"
+  puts "One energetic etymolygist to guess the phrase (Player 2)"
+
+  puts "\nWho gets to choose the phrase for this game? (Player 1)"
+  player1 = gets.chomp
+
+  puts "\nWho will guess the phrase for this game? (Player 2)"
+  player2 = gets.chomp
+
+  puts "\nGreat! Glad that's decided. #{player2}, close your eyes now."
+  puts "#{player1}, enter your phrase now:"
+  phrase = gets.chomp
+
+  game = WordGame.new(phrase, player1, player2)
+
+  until game.game_over
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "#{game.player2}, you have #{game.guesses_allowed - game.guesses_made} guesses remaining."
+    puts "Please enter your next guess:"
+    game.user_guess(gets.chomp)
+  end
+
+  puts "\nWould you like to play again?\n"
+  if ['n', 'no'].include? gets.chomp.downcase
+    break 
   end
 
 end

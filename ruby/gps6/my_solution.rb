98b2285985945cf6b50f8384c7a73af369e99dff
Robert Turner Jr <georob23@gmail.com>
2016-09-12 22:13:44 -0700

    Refactor speed_of_spread method to make it DRYer.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -65,17 +65,26 @@ class VirusPredictor
     # by additional factors we haven't added into this functionality.
     speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
+    case @population_density
+      when 200..10000 then speed = 0.5
+      when 150...200 then speed = 1
+      when 100...150 then speed = 1.5
+      when 50...100 then speed = 2
     else
-      speed += 2.5
+      speed = 2.5 
     end
+
+    # if @population_density >= 200
+    #   speed += 0.5
+    # elsif @population_density >= 150
+    #   speed += 1
+    # elsif @population_density >= 100
+    #   speed += 1.5
+    # elsif @population_density >= 50
+    #   speed += 2
+    # else
+    #   speed += 2.5
+    # end
 
     puts " and will spread across the state in #{speed} months.\n\n"
 

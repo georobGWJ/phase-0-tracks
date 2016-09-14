9fda06bbac24cad9738df90e66a67702a1bb82ba
Robert Turner Jr <georob23@gmail.com>
2016-09-14 10:38:01 -0700

    Write pseudocode for JavaScript Reverse String function.

---
+++ js/explore.js
@@ -0,0 +1,19 @@
+// DBC Challenge 7.1
+// JavaScript Reverse String Function
+
+// Define Function reverse_string
+//   This function takes a string as an argument and returns a reversed string.
+
+// Assign variable to hold string
+
+// Assign variable with an empty string to hold the reversed string
+
+// Use a for while loop to iterate through input string
+//   Since the characters in the string can be accessed via their index values,
+//   the loop will start at the end of the index string and iterate towards
+//   the beginning.
+//   As each character is accessed, it will be concatenated onto the 
+//   return string variable.
+
+// Explicity return the reversed string variable
+

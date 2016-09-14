152cf4dd058f8cd4a76bb421e66f9d5e4cc7b133
Robert Turner Jr <georob23@gmail.com>
2016-09-14 16:36:49 -0700

    Write Pseudocode for longest element in an array function.

---
+++ js/algos.js
@@ -0,0 +1,24 @@
+// DBC Challenge 7.3
+// Release 0: Write a function that takes an array of words or phrases and 
+// returns the longest word or phrase in the array. 
+
+// So if we gave your function the array of 
+// ["long phrase","longest phrase","longer phrase"], it would return 
+// "longest phrase". 
+
+// Release 0 PSEUDOCODE
+//   This function takes an array of string elements and returns a string
+
+// Define function longest_element
+//   This function takes an array of string elements and returns a string
+
+//   Create an empty String variable to store the longest word or phrase
+
+//   Use a for while loop to iterate through input array.
+//     For each element, check it's length.
+//     If the length is longer than the existing longest word variable,
+//       replace the existing longest word variable with the current element.
+
+//  Explicitly return the longest word phrase
+
+// Release 0 IMPLEMENTED CODE

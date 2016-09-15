6c6ca642faf4e2d73d30d3658806ead7863075e3
Robert Turner Jr <georob23@gmail.com>
2016-09-15 09:03:04 -0700

    Implement Release 0 code.

---
+++ js/data_structure.js
@@ -0,0 +1,11 @@
+// DBC Challenge 7.2
+
+var colors = ['blue', 'indigo', 'silver', 'black'];
+var horses = ['Pinky', 'Brain', 'Robert', 'Julia'];
+
+colors.push('auburn');
+horses.push('Miguel');
+
+// Test Driver Code
+console.log(colors);
+console.log(horses);

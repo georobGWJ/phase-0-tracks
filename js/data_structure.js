ef80fbd450ef2abfc81fc63616041cabdd4d6637
Robert Turner Jr <georob23@gmail.com>
2016-09-15 09:32:33 -0700

    Create Car class / function for Release 2.

--- js/data_structure.js
+++ js/data_structure.js
@@ -13,6 +13,26 @@ for (counter = 0; counter < colors.length; counter++) {
   horse_colors[horses[counter]] = colors[counter];
 }
 
+function Car(color, size, type) {
+  console.log('Our new car:', this);
+
+  this.color = color;
+  this.size = size;
+  this.type = type;
+
+  this.speed = function(mph) {
+    console.log('This car is going ' + mph + ' miles per hour.');
+  }
+}
+
+var sedan = new Car('red', 'big', '4-door');
+console.log(sedan);
+sedan.speed(55);
+console.log('------')
+
+var truck = new Car('black', 'compact', 'pick-up');
+console.log(truck);
+truck.speed(40);
 
 // Test Driver Code
 // console.log(colors);

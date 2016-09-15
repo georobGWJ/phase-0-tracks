0175d9b5c30d830722e1a02a06afec11237da247
Robert Turner Jr <georob23@gmail.com>
2016-09-14 18:47:52 -0700

    Write Release 2 Pseudocode. Clean up formatting.

--- js/algos.js
+++ js/algos.js
@@ -1,9 +1,10 @@
 // DBC Challenge 7.3
+
+//==========================================================
 // RELEASE 0: Write a function that takes an array of words or phrases and 
 // returns the longest word or phrase in the array. 
 
-
-// Release 0 PSEUDOCODE
+// RELEASE 0 PSEUDOCODE
 
 // Define function longest_element
 //   This function takes an array of string elements and returns a string
@@ -17,7 +18,8 @@
 
 //  Explicitly return the longest word phrase
 
-// Release 0 IMPLEMENTED CODE
+//==========================================================
+// RELEASE 0 IMPLEMENTED CODE
 function find_longest(phrases) {
   var longest_phrase = '';
 
@@ -29,10 +31,11 @@ function find_longest(phrases) {
   return longest_phrase
 }
 
+//==========================================================
 // RELEASE 1: Write a function that takes two objects and checks to see if the 
 // objects share at least one key-value pair. 
 
-// Release 1 PSEUDOCODE
+// RELEASE 1 PSEUDOCODE
 // Define function key_value_match
 //   This function takes two object and looks for matching key-value pairs. If
 //   at least one matching pair is found the function returns True, otherwise
@@ -50,7 +53,8 @@ function find_longest(phrases) {
 
 //   Explicity return the matching pair flag
 
-// Release 1 IMPLEMENTED CODE
+//==========================================================
+// RELEASE 1 IMPLEMENTED CODE
 function key_value_match(object1, object2) {
   var match_flag = false;
   var object1_keys = Object.keys(object1);
@@ -67,6 +71,52 @@ function key_value_match(object1, object2) {
 return match_flag
 }
 
+
+//==========================================================
+// RELEASE 2: Write a function that takes an integer for length, and builds 
+// and returns an array of strings of the given length. So if we ran your 
+// function with an argument of 3, we would get an array of 3 random words back 
+// (the words don't have to be actual sensical English words -- "nnnnfph" 
+// totally counts). The words should be of randomly varying length, with a 
+// minimum of 1 letter and a maximum of 10 letters.
+
+// RELEASE 2 PSEUDOCODE
+
+// Define function random_words function
+//   This function takes an integer and returns an array of string elements 
+//   as long as the input argument. Each element in the return array varies
+//   randomly between 1 and 10 characters.
+
+//   Create an empty array variable that will hold the words and be returned
+//   Create an alphabet string. This will be randomly sampled to build fake words
+
+//     Create a for-while loop that iterates once for every word that is to be 
+//       created.
+//       Create an empty string variable to store the fake word
+
+//         Randomly select a value between 1 and 10
+//           Create a nested for-while loop to build the fake word that iterated
+//           the number of times the RNG above generated.
+
+//           Randomly generate another number between 1 and the length of the fake
+//           letter list. 
+
+//       Access the letter and concatenate it to the fake word
+
+//     Explicity return the array of fake words.
+
+//==========================================================
+// RELEASE 2 IMPLEMENTED CODE
+
+function random_words(num_of_words) {
+  
+}
+
+//==========================================================
+// RELEASE 2 Test Driver Code
+
+
+//==========================================================
 // RELEASE 1 Test Driver Code
 // cat1 = {name: 'Tom', age: 7, fav_food: 'mice'}
 // cat2 = {name: 'Heathcliff', age: 7, fav_food: 'mice'}
@@ -77,9 +127,9 @@ return match_flag
 // console.log(key_value_match(cat1, mouse1))
 // console.log(key_value_match(mouse1, mouse2))
 
+
+//==========================================================
 // RELEASE 0 Test Driver Code
 // some_words = ['cheese', 'cantaloupe', 'catamaran', 'cirronimbus clouds', 'cat']
 
 //  console.log("The longest word or phrase is '" + find_longest(some_words) + "'.")
-
- // RELEASE 1 Test Driver Code

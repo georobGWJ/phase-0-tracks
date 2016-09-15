06cdfefc5ea3800e0e069fd691ae3c3db52f3a33
Robert Turner Jr <georob23@gmail.com>
2016-09-15 10:01:54 -0700

    Add test Object loop code. Clean up comments.

--- js/data_structure.js
+++ js/data_structure.js
@@ -1,4 +1,6 @@
 // DBC Challenge 7.2
+// @juliadolgoszyja (Slack) (@yulkalongneck on GitHub) and I paired for this
+// Challenge on Sep 15, 2016
 
 var colors = ['blue', 'indigo', 'silver', 'black'];
 var horses = ['Pinky', 'Brain', 'Robert', 'Julia'];
@@ -13,7 +15,7 @@ for (counter = 0; counter < colors.length; counter++) {
   horse_colors[horses[counter]] = colors[counter];
 }
 
-function Car(color, size, type) {
+function Car(color, size, type = 'default type') {
   console.log('Our new car:', this);
 
   this.color = color;
@@ -33,6 +35,19 @@ console.log('------')
 var truck = new Car('black', 'compact', 'pick-up');
 console.log(truck);
 truck.speed(40);
+console.log('------')
+
+var test = new Car('purple', 'midsize');
+console.log(test);
+test.speed(44);
+
+// Test object loop code
+// var sedan_keys = Object.keys(sedan);
+
+// for (counter = 0; counter < sedan_keys.length; counter++) {
+//   console.log(sedan_keys[counter] + ' : ' + sedan[sedan_keys[counter]]);
+// }
+
 
 // Test Driver Code
 // console.log(colors);

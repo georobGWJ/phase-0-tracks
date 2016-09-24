987210b0a3dc00d9b1cbc35212e306d011cb99d1
Robert Turner Jr <georob23@gmail.com>
2016-09-24 10:55:39 -0700

    Create skeleton helper methods.

---
+++ ruby/persistent-app/geo_data.rb
@@ -0,0 +1,42 @@
+# geo_data.rb
+# A program that creates and manipulates a database of soil sample data,
+# rig geologist data, borehole data and project data.
+# Robert Turner
+
+require 'sqlite3'
+require 'tk' # I hope to implement a GUI, time permitting
+
+# Helper Methods
+
+# Create a brand new empty database
+def create_db(db_name)
+
+end
+
+# Load an existing database
+def open_db(name)
+
+end
+
+# View all single table data
+def view_table(table)
+end
+
+# Add Project to database
+def add_project()
+end
+
+# Add Borehole to database
+def add_project()
+end
+
+# Add geologist to database
+def add_project()
+end
+
+# Add sample to database
+def add_project()
+end
+
+
+# TEST DRIVER CODE

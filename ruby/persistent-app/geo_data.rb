75d63e70ca3ba1da67715aa4302300550c971208
Robert Turner Jr <georob23@gmail.com>
2016-09-24 11:38:05 -0700

    Implement create_db method. Add test.db to .gitignore.

--- ruby/persistent-app/geo_data.rb
+++ ruby/persistent-app/geo_data.rb
@@ -10,7 +10,67 @@ require 'tk' # I hope to implement a GUI, time permitting
 
 # Create a brand new empty database
 def create_db(db_name)
+  db = SQLite3::Database.new(db_name + ".db")
+  db.results_as_hash = true
 
+  # Create Project Table
+  create_project_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS projects(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255) UNIQUE,
+    client VARCHAR(255)
+  )
+  SQL
+
+  # Create Geologists Table
+  create_geo_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS geologists(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255) UNIQUE,
+    company VARCHAR(255)
+  )
+  SQL
+
+  # Create Borehole Table
+  create_borehole_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS boreholes(
+    id INTEGER PRIMARY KEY,
+    designation VARCHAR(255) UNIQUE,
+    northing REAL,
+    easting REAL,
+    elevation REAL,
+    project_id INTEGER,
+    FOREIGN KEY (project_id) REFERENCES projects(id)
+  )
+  SQL
+  
+  # Create Samples Table
+  create_sample_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS samples(
+    id INTEGER PRIMARY KEY,
+    bh_id INTEGER, 
+    logger_id INTEGER,
+    depth REAL,
+    USCS VARCHAR(63),
+    color VARCHAR(127),
+    wetness VARCHAR(63),
+    percent_gravel INTEGER,
+    percent_sand INTEGER,
+    percent_fines INTEGER,
+    plasticity VARCHAR(63),
+    toughness VARCHAR(63),
+    other VARCHAR(255),
+    FOREIGN KEY (bh_id) REFERENCES boreholes(id),
+    FOREIGN KEY (logger_id) REFERENCES geologists(id)
+  )
+  SQL
+
+  db.execute(create_project_table)
+  db.execute(create_geo_table)
+  db.execute(create_borehole_table)
+  db.execute(create_sample_table)
+
+  return db
 end
 
 # Load an existing database
@@ -40,3 +100,4 @@ end
 
 
 # TEST DRIVER CODE
+create_db("test")

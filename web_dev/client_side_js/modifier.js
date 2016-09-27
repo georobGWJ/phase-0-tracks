25f0f29ca27958890652f598c85a99564e3887ce
Robert Turner Jr <georob23@gmail.com>
2016-09-27 09:02:08 -0700

    Add Event Listener for Release 2.

--- web_dev/client_side_js/modifier.js
+++ web_dev/client_side_js/modifier.js
@@ -1,7 +1,16 @@
-var div_para = document.getElementsByTagName("p");
+// Change font styles using JavaScript and DOM Elements
+var para = document.getElementsByTagName("p");
 
-console.log(div_mod);
+para[0].style.fontFamily = "Helvetica";
 
-div_para[0].style.fontFamily = "Helvetica";
+para[0].style.fontSize = "40px"
 
-div_para[0].style.fontSize = "40px"
+// Add Event Listener using JavaScript and DOM Elements
+var lizard = document.getElementById("lizard-photo")
+
+function addBorder() { 
+  var lizard = document.getElementById("lizard-photo");
+  lizard.style.border = "2px solid green"; 
+}
+
+lizard.addEventListener("mouseover", addBorder)
